03e3b7ab324d3b483fde385fb2c1bf7dee061334
josemartinmolina <jose.molina@tec.mx>
2022-03-15 19:50:25 -0600

    Initial commit

---
+++ videogame/urls.py
@@ -0,0 +1,9 @@
+from unicodedata import name
+from django.urls import path
+from . import views
+
+
+urlpatterns = [path('', views.index, name='index'), 
+    path('proceso',views.proceso, name='proceso'),
+    path('datos', views.datos, name='datos'),
+]
